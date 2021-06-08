5817895a476160e3b9132f687620ef7c736a9ab4
Luke Hutchison <luke.hutch@gmail.com>
2021-06-08 02:36:57 -0600

    Update JavaDoc

--- src/main/java/squirrelparser/clause/terminal/Regexp.java
+++ src/main/java/squirrelparser/clause/terminal/Regexp.java
@@ -8,6 +8,11 @@ import squirrelparser.parser.Parser;
 /**
  * Matches if the regexp matches, starting at the current position. The length of the match is the number of
  * characters matched by the regexp.
+ * 
+ * <p>
+ * This {@link Terminal} may be thought of as a sort of lexer or tokenizer, and may result in faster parsing with
+ * lower memory requirements than if the token parsing is built using PEG rules. PEG rules may result in one
+ * {@link Match} (and therefore one memo entry) per character of each token.
  */
 public class Regexp extends Terminal {
     private final String regexp;
