31b9eafab1b3260d4f04ef26ecc1cc867954a9ee
Luke Hutchison <luke.hutch@gmail.com>
2021-06-08 00:06:22 -0600

    Improve output

--- src/main/java/squirrelparser/main/Main.java
+++ src/main/java/squirrelparser/main/Main.java
@@ -20,19 +20,22 @@ public class Main {
     }
 
     public static void main(String[] args) throws Exception {
-        var grammar = MetaGrammar.parse(loadResourceFile("arithmetic.grammar"));
-        System.out.println("Grammar:\n" + grammar);
-
         var input = loadResourceFile("arithmetic.input");
-        System.out.println("\nInput:\n" + input);
+        System.out.println("INPUT:\n\n" + input);
 
-        var parser = new Parser(grammar, input);
+        var grammarSource = loadResourceFile("arithmetic.grammar");
+        System.out.println("\nGRAMMAR SOURCE:\n\n" + grammarSource);
+
+        var rewrittenGrammar = MetaGrammar.parse(grammarSource);
+        System.out.println("\nREWRITTEN GRAMMAR:\n\n" + rewrittenGrammar);
+
+        var parser = new Parser(rewrittenGrammar, input);
         var match = parser.parse();
-        System.out.println("\nParse tree:\n" + match.toStringWholeTree(parser.input));
+        System.out.println("\nPARSE TREE:\n\n" + match.toStringWholeTree(parser.input));
 
         if (match != Match.NO_MATCH) {
             var ast = new ASTNode(match, parser.input);
-            System.out.println("\nAST:\n" + ast.toStringWholeTree());
+            System.out.println("\nAST:\n\n" + ast.toStringWholeTree());
         }
     }
 }
