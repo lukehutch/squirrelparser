5e095265a0d4563a518579f239b7f0fcd4dc0aa2
Luke Hutchison <luke.hutch@gmail.com>
2021-06-08 00:35:04 -0600

    Reduce size of parse tree

--- src/main/java/squirrelparser/clause/nonterminal/RuleRef.java
+++ src/main/java/squirrelparser/clause/nonterminal/RuleRef.java
@@ -5,6 +5,7 @@ import squirrelparser.node.Match;
 import squirrelparser.parser.Parser;
 import squirrelparser.rule.Rule;
 
+/** Matches if the named rule matches at the current position. */
 public class RuleRef extends Clause {
     public String refdRuleName;
     public Rule refdRule;
@@ -17,12 +18,15 @@ public class RuleRef extends Clause {
     public Match match(int pos, int rulePos, Parser parser) {
         // Match the referenced rule
         var refdRuleMatch = refdRule.match(pos, rulePos, parser);
-        if (refdRuleMatch != Match.NO_MATCH) {
-            // Wrap the match of the referenced rule in a new match, so that RuleRefs can add their own
-            // AST node labels to the AST, even if the referenced rule has its own AST node label
-            return new Match(this, /* subClauseMatch = */ refdRuleMatch);
+        if (astNodeLabel == null) {
+            // This RuleRef clause doesn't have a separate AST node label; just reuse the Match from the
+            // referenced rule
+            return refdRuleMatch;
         } else {
-            return Match.NO_MATCH;
+            // Wrap the match of the referenced rule in a new Match, so that this RuleRef's AST node label
+            // can be recorded in the parse tree
+            return refdRuleMatch == Match.NO_MATCH ? Match.NO_MATCH
+                    : new Match(this, /* subClauseMatch = */ refdRuleMatch);
         }
     }
 
