7320c0e856024cdab78182cec6ca8a10295eef92
Luke Hutchison <luke.hutch@gmail.com>
2021-06-18 16:03:39 -0600

    Refactoring

--- src/main/java/squirrelparser/parser/Parser.java
+++ src/main/java/squirrelparser/parser/Parser.java
@@ -50,82 +50,8 @@ public class Parser {
     public Match match(Clause rule, int pos, int parentRuleStart) {
         // Get the existing memo entry for this rule and position, if there is one, otherwise add
         // a new blank memo entry to the memo table for this rule and position.
-        var rulePos = new RulePos(rule, pos);
-        var memoEntry = memoTable.computeIfAbsent(rulePos, rp -> new MemoEntry());
-
-        // If parent recursion frame and this recursion frame have different start positions,
-        // and there is a memo entry for this rule and position, return the memo entry rather
-        // than duplicating work. (If pos == parentRuleStart, don't look at the memo table,
-        // so that when expanding left-recursive cycles, lower matches don't prevent the
-        // detection of higher matches in the parse tree.)
-        if (pos != parentRuleStart && memoEntry.match != null) {
-            return memoEntry.match;
-        }
-
-        // If we encountered the same rule and position twice in the recursion path, then we reached
-        // a left-recursive cycle
-        if (memoEntry.inRecPath) {
-            // Mark this memo entry as being the "head node" in the left recursive cycle.
-            // This effectively passes a message back up to the ancestral node with the same
-            // rule and position to tell it to start iteratively growing the left-recursive
-            // subtree of the parse tree.
-            memoEntry.inLeftRecCycle = true;
-            // When a left-recursive cycle is first found, the bottom-most node in the parse
-            // tree corresponding to this clause at this position must be marked as a mismatch. 
-            // However, if a better match was already found (e.g. because the ancestral "head of
-            // the cylce" has iterated more than once), don't override that better match with
-            // a mismatch.
-            if (memoEntry.match == null) {
-                memoEntry.match = Match.MISMATCH;
-            }
-            // Return memo entry for current-best match (starting with mismatch)
-            return memoEntry.match;
-        }
-
-        // Otherwise no memo entry is available or usable, and no left recursive cycle has yet been detected --
-        // recurse on this rule and position (i.e. perform standard recursive descent parsing).
-        memoEntry.inRecPath = true;
-        // No left recursion cycle is yet known
-        memoEntry.inLeftRecCycle = false;
-        
-        // Left recursion expansion loop (executes only once if there no left recursive cycle is encountered).
-        while (true) {
-            // Try matching this rule's toplevel clause at this position.
-            // newMatch will be either MISMATCH if there was no match, or a Match reference if there was a match.
-            // Record the start position of the rule as the current position, so that when a RuleRef causes
-            // recursion to return back to this method, whether or not the memo table should be consulted
-            // can be checked (via the pos != parentRuleStart test, above).
-            var newMatch = rule.match(this, pos, /* ruleStart = */ pos);
-
-            // Stop iterating if newMatch is not longer than bestMatch. This ensures this cycle will terminate,
-            // because a left-recursive cycle must consume at least one extra character per loop around the cycle.
-            // This is also true for First clauses, for very subtle reasons:
-            // https://github.com/lukehutch/pikaparser/issues/32#issuecomment-861873964
-            // N.B. Match.MISMATCH was defined to have a sentinel length of -1, so that even a zero-length match
-            // always beats MISMATCH.
-            if (memoEntry.match != null && newMatch.len <= memoEntry.match.len) {
-                // Match did not monotonically improve -- don't memoize newMatch (and stop iterating, if iterating)
-                break;
-            }
-
-            // Write the new or improved match to the memo table
-            memoEntry.match = newMatch;
-
-            // Check if this recursion frame was marked for iteration by a lower recursion frame
-            // of the same rule and position detecting a left-recursive cycle had been reached
-            // (see where this flag was set, above).
-            if (!memoEntry.inLeftRecCycle) {
-                // Not in a left-recursive cycle -- don't iterate.
-                break;
-            } // else keep iteratively matching until the match can no longer be improved. 
-        }
-
-        // On exit from this frame of recursion, mark this rule and position as is no longer being part of
-        // the recursion path. (This is paired with "memoEntry.inRecPath = true;" before the while loop.) 
-        memoEntry.inRecPath = false;
-
-        // Return the best match so far
-        return memoEntry.match;
+        var memoEntry = memoTable.computeIfAbsent(new RulePos(rule, pos), rp -> new MemoEntry());
+        return memoEntry.match(this, rule, pos, parentRuleStart);
     }
 
     /** Start parsing from the top rule at the beginning of the input. */
