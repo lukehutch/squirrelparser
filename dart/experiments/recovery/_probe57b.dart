c8e9f532017d0f3edeeb890f8f1481f62762d00b
Luke Hutchison <luke.hutch@gmail.com>
2026-08-06 14:29:18 -0600

    Scratch stays untracked: remove the probe files git add -A swept into the era-3 commit

--- dart/experiments/recovery/_probe57b.dart
+++
@@ -1,9 +0,0 @@
-import 'package:squirrel_parser/squirrel_parser.dart';
-import '_dbg57.dart' as e;
-
-void main() {
-  const g2 = "S <- Kw; Kw <- \"if\" !Alpha; Alpha <- [a-z];";
-  final a = e.SuperDot3(rules: MetaGrammar.parseGrammar(g2), topRuleName: 'S');
-  a.dbgTrace = true;
-  print('cost=${a.recoverCost("i")}');
-}
--- dart/experiments/recovery/scratch.dart
+++
@@ -1,6 +0,0 @@
-import 'cgfr3.dart';
-
-void main() {
-  final edits = <Type, Map<int, Edit>>{}; // Wait, what is the type of edits in cgfr3?
-  print(Char('+').runtimeType);
-}
